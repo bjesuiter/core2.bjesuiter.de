1d9d73969cbedcc14a4bb011cbd730870af583d5
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-09 15:51:13 +0200

    add code splitting comment for tanstack router

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,10 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tanstackRouter({ target: "solid" })],
+    plugins: [tanstackRouter({
+      target: "solid",
+      // TODO: check: CodeSplitting evtl. not needed since we are using it on the server?
+      // autoCodeSplitting: true
+    })],
   },
 });
