da40b14e23601ba54ad44f934c664824d6322e2c
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-14 10:51:54 +0200

    format index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/solid-router";
 import Counter from "~/components/Counter";
 
 export const Route = createFileRoute("/")({
-  component: RouteComponent
+  component: RouteComponent, 
 });
 
 function RouteComponent() {
